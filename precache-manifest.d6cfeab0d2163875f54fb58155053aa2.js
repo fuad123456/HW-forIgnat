bdf59f5345aa18f39cf7458716040f33c5410a62
Fuad <fuadkrym@gmail.com>
2022-09-27 17:50:19 +0300

    Updates

---
+++ precache-manifest.d6cfeab0d2163875f54fb58155053aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96620a3fa656b8b11f127caa80a38bc4",
+    "url": "/HW-forIgnat/index.html"
+  },
+  {
+    "revision": "46509a376731734af911",
+    "url": "/HW-forIgnat/static/css/main.08393b59.chunk.css"
+  },
+  {
+    "revision": "e3943099cfc42689d3d7",
+    "url": "/HW-forIgnat/static/js/2.f8a5e7b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/HW-forIgnat/static/js/2.f8a5e7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46509a376731734af911",
+    "url": "/HW-forIgnat/static/js/main.87345fd5.chunk.js"
+  },
+  {
+    "revision": "b4bda2fa2014f8800385",
+    "url": "/HW-forIgnat/static/js/runtime-main.f056a9f9.js"
+  }
+]);
